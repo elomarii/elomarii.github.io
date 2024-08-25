ecbf518e2f4ce3a2c03cb0c02edc26df6cc3cdd6
elomarii <i.ibrahim.elomari@gmail.com>
2024-08-25 16:49:04 +0200

    init

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1724178814',resources: [
+      '/assets/css/elomarii-portfolio.css',
+      '/',
+      
+        '/projects/',
+      
+        '/blog/',
+      
+        '/tags/',
+      
+        '/whoami/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
